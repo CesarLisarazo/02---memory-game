2e412fa923bea1ba48275469afb4b8d120393fb6
CesarLisarazo <cesarlisarazo@gmail.com>
2024-09-05 22:38:49 -0300

    second commit

--- script.js
+++ script.js
@@ -51,25 +51,86 @@ const cardArray = [
 
 ]
 
+
+
+
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.getElementById('grid')
+let score = document.getElementById('score')
+let cardsChosen = []
+let cardsChosenIds = []
+let cardsWon = []
+let card
+let clicks = document.getElementById('clicks')
 
-function createBoard (){
-    for(let i=0; i< 12; i++ ){
-        const card=document.createElement('img')
-        card.setAttribute('src','images/card.jpeg')
-        card.setAttribute('data-id',i)
-        card.addEventListener('click',flipCard)
+
+
+
+function createBoard() {
+    for (let i = 0; i < 12; i++) {
+        let card = document.createElement('img')
+        card.setAttribute('src', 'images/card.jpeg')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
-
     }
 }
+
+
+
 createBoard()
 
 
-function flipCard(){
-   const cardId = this.getAttribute('data-id')
 
-console.log(cardArray, cardArray[cardId].name)
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+
+    if (optionOneId == optionTwoId) {
+        alert('Hiciste click en la misma imagen, lo arruinaste todo! \n yava llorá')
+        location.reload();
+    }
+
+    if (cardsChosen[0] == cardsChosen[1]) {
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cards[optionOneId])
+        cardsWon.push(cards[optionTwoId])
+        console.log(cardsWon)
+        score.innerHTML = cardsWon.length / 2
+
+    }
+    else {
+
+        cards[optionOneId].setAttribute('src', 'images/card.jpeg')
+        cards[optionTwoId].setAttribute('src', 'images/card.jpeg')
+    }
+
+    if (score.innerHTML == cardArray.length / 2) {
+        alert('Ganaste wey!')
+        location.reload();
+    }
+
+    cardsChosen = []
+    cardsChosenIds = []
 }
+
+
+
+
+
+function flipCard() {
+    clicks.innerHTML = Number(clicks.innerHTML) + 1
+    const cardId = this.dataset.id
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
+
