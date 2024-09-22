e54d57fae85102c70614518fe2e2ced41935982c
CesarLisarazo <cesarlisarazo@gmail.com>
2024-09-22 12:05:30 -0300

    br

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ let reloadGame=document.getElementById("reloadGame")
 reloadGame.addEventListener("click",clearDisplay)
 Swal.fire({
   
-    html:  " <br><br><br><br><br><br><br><span id='homeSoundOnOff' class='homeText'>Sound Off / On<br></span><button id='homeSound'>🔊</button><br><br><span class='homeText'>English / Español</span><br><button id='homeIdiom'>English</button><br><br><br>",
+    html:  " <br><br><br><br><br><br><span id='homeSoundOnOff' class='homeText'>Sound Off / On<br></span><button id='homeSound'>🔊</button><br><br><span class='homeText'>English / Español</span><br><button id='homeIdiom'>English</button><br><br><br>",
     confirmButtonText: "Start",
    // Fondo del modal transparente
     width:"40vh",
